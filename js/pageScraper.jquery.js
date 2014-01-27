d58944bcb7802b8efa08e0dc9ad86d82154aeb65
Nicole Paz <nicole.j.paz@gmail.com>
2014-01-27 15:06:20 -0800

    Added defaults and options.

--- js/pageScraper.jquery.js
+++ js/pageScraper.jquery.js
@@ -1,5 +1,12 @@
 (function ($) {
-  $.fn.pageScraper = function() {
-    
+  $.fn.pageScraper = function(options) {
+  	var defaults = {
+      element: '#link_holder'
+    },
+    settings = $.extend({}, defaults, options)
+
+    this.each(function() {
+    	
+    })
   }
 }(jQuery))
